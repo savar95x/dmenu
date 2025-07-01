8bae41f6e11894bd33af5fe06a867ddb39c1746c
savar95x <skaar513@gmail.com>
2025-07-02 01:07:43 +0530

    update

--- config.def.h
+++ config.def.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static const unsigned int alpha = 0xff;     /* Amount of opacity. 0xff is opaque             */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const int user_bh = 10;               /* add an defined amount of pixels to the bar height */
+static const int user_bh = 20;               /* add an defined amount of pixels to the bar height */
 
 static const char *fonts[] = {
 	"monospace:size=11"
